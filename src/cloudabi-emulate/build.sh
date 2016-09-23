eacccfb768407cc4a8006a662293cf6f38767a11
Ed Schouten <ed@nuxi.nl>
2016-09-23 14:10:56 +0200

    Simplify the build script.

--- src/cloudabi-emulate/build.sh
+++ src/cloudabi-emulate/build.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 x86_64-unknown-cloudabi-cc -O2 -Wall -Werror -I../libemulator \
-    -o cloudabi-emulate \
-    ../libemulator/emulate.c \
-    ../libemulator/futex.c \
-    ../libemulator/posix.c \
-    ../libemulator/random.c \
-    ../libemulator/signals.c \
-    ../libemulator/str.c \
-    ../libemulator/tidpool.c \
-    ../libemulator/tls.c \
-    cloudabi-emulate.c
+    -o cloudabi-emulate cloudabi-emulate.c ../libemulator/*.c
